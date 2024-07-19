5244f78fb46c6b8c2e72be09ab7a98ea52c68a87
GinsengGhoul <ongteh@protonmail.com>
2024-07-19 14:47:56 -0700

    yay red window

--- src/gypsoid.cxx
+++ src/gypsoid.cxx
@@ -1,69 +1,64 @@
-#include <SDL.h>
-#include <SDL_opengl.h>
-#include <cmath>
+#include <SDL3/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+using namespace std;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+SDL_Window* window = 0;
 
-void drawTorus(float radius1, float radius2, int numc, int numt) {
-  float TWO_PI = 2 * 3.14159265358979323846;
-  float alpha, beta;
-  float x, y, z;
+int main(int argc, char* args[]){
+  // display 0 is primary display
+  int windowPosX = SDL_WINDOWPOS_CENTERED_DISPLAY(0);
+  int windowPosY = SDL_WINDOWPOS_CENTERED_DISPLAY(0);
+  int width = 800;
+  int height = 600;
+  const char* title = "wowza it's a title";
+  // or the flags together
+  uint flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
-  for (int i = 0; i < numc; i++) {
-    glBegin(GL_QUAD_STRIP);
-    for (int j = 0; j <= numt; j++) {
-      for (int k = 0; k < 2; k++) {
-        alpha = (i + k) % numc * TWO_PI / numc;
-        beta = j % numt * TWO_PI / numt;
-        x = (radius1 + radius2 * cos(beta)) * cos(alpha);
-        y = (radius1 + radius2 * cos(beta)) * sin(alpha);
-        z = radius2 * sin(beta);
-        glVertex3f(x, y, z);
+  if(SDL_Init(SDL_INIT_EVENTS) >= 0){
+    SDL_PropertiesID props = SDL_CreateProperties();
+    SDL_SetStringProperty(props, SDL_PROP_WINDOW_CREATE_TITLE_STRING, title);
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_X_NUMBER, SDL_WINDOWPOS_CENTERED_DISPLAY(0));
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_Y_NUMBER, SDL_WINDOWPOS_CENTERED_DISPLAY(0));
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, width);
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, height);
+    SDL_SetNumberProperty(props, "flags", flags);
+
+    window = SDL_CreateWindowWithProperties(props);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    float red = 0.0f;
+    float blu = 0.0f;
+    float grn = 0.0f;
+    int frame{};
+
+    bool isRunning = true;
+    while(isRunning){
+
+      red += 0.1f;
+      if(red > 1.1f){
+        red = 0.0f;
+      }
+      glClearColor(red, blu, grn, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      SDL_GL_SwapWindow(window);
+      SDL_Event event;
+      while (SDL_PollEvent(&event)){
+        if(event.type == SDL_EVENT_QUIT){
+          isRunning = false;
+        }
       }
     }
-    glEnd();
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
+    return 0;
+
+  }
+  else {
+    return 1;
   }
 }
 
-void render() {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glLoadIdentity();
-  glTranslatef(0.0f, 0.0f, -5.0f);
-  glRotatef(SDL_GetTicks() * 0.01f, 1.0f, 0.0f, 0.0f);
-  glRotatef(SDL_GetTicks() * 0.01f, 0.0f, 1.0f, 0.0f);
-  glRotatef(SDL_GetTicks() * 0.01f, 0.0f, 0.0f, 1.0f);
-  glColor3f(0.0f, 1.0f, 0.0f);
-  drawTorus(1.0f, 0.3f, 20, 20);
-  SDL_GL_SwapWindow(window);
-}
-
-int main(int argc, char* argv[]) {
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window* window = SDL_CreateWindow("Spinning Torus",
-    SCREEN_WIDTH, SCREEN_HEIGHT,
-    SDL_WINDOW_OPENGL | 0);
-  SDL_GLContext context = SDL_GL_CreateContext(window);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(45.0f, (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 100.0f);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glEnable(GL_DEPTH_TEST);
-
-  bool running = true;
-  SDL_Event event;
-  while (running) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT)
-        running = false;
-    }
-    render();
-  }
-
-  SDL_GL_DeleteContext(context);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return 0;
-}
